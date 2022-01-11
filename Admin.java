dcfa64be01d50fa1f5ed838e603e887ac11eb6d6
WrathOfXLord <61274689+WrathOfXLord@users.noreply.github.com>
2022-01-11 23:45:14 +0300

    -added-
    Main screen and admin login screen created.

---
+++ Admin.java
@@ -0,0 +1,117 @@
+package DatabaseProject;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Arrays;
+
+public class Admin extends JFrame {
+
+	private JPanel contentPane;
+	private JPasswordField passwordField;
+	private static String userName = "swiftending";
+	private static String password = "111111";
+	private Administration admin;
+	
+	public Admin() {
+		setTitle("Administration - Tetra Corporation");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 400, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(153, 0, 51));
+		panel.setBounds(0, 0, 394, 371);
+		contentPane.add(panel);
+		
+		JLabel lblUsername = new JLabel("Kullan\u0131c\u0131 Ad\u0131");
+		lblUsername.setBounds(51, 148, 126, 31);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblUsername.setForeground(new Color(255, 255, 255));
+		lblUsername.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JFormattedTextField usernameField = new JFormattedTextField();
+		usernameField.setBounds(181, 152, 136, 23);
+		usernameField.setBackground(new Color(255, 255, 255));
+		usernameField.setFont(new Font("Tahoma", Font.BOLD, 12));
+		
+		JLabel lblUsername_1 = new JLabel("Parola");
+		lblUsername_1.setBounds(51, 190, 126, 31);
+		lblUsername_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUsername_1.setForeground(new Color(255, 255, 255));
+		lblUsername_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(181, 195, 136, 22);
+		passwordField.setBackground(new Color(255, 255, 255));
+		
+		JButton btnGiris = new JButton("Giri\u015F");
+		btnGiris.setBounds(252, 254, 65, 23);
+		btnGiris.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(usernameField.getText().isEmpty()) {
+					JOptionPane.showMessageDialog(null, "Kullanıcı Adı Girin !", 
+							  "Boş Kullanıcı Adı !", JOptionPane.ERROR_MESSAGE);
+				} else if (Arrays.equals("".toCharArray(), passwordField.getPassword())) {
+					JOptionPane.showMessageDialog(null, "Parola Girin !", 
+							  "Boş Parola !", JOptionPane.ERROR_MESSAGE);
+				} else if(!usernameField.getText().equals(Admin.userName)) {
+					JOptionPane.showMessageDialog(null, "Hatalı Kullanıcı Adı veya Parola !", 
+							  "Hatalı Girdi !", JOptionPane.ERROR_MESSAGE);
+				} else if(!Arrays.equals(Admin.password.toCharArray(), passwordField.getPassword())) {
+					JOptionPane.showMessageDialog(null, "Hatalı Kullanıcı Adı veya Parola !", 
+							  "Hatalı Girdi !", JOptionPane.ERROR_MESSAGE);
+				} else {
+					admin = new Administration();
+					admin.setVisible(true);
+					dispose();
+				}
+			}
+		});
+		btnGiris.setForeground(new Color(255, 255, 255));
+		btnGiris.setBackground(new Color(28, 32, 40));
+		
+		JButton btnGeri = new JButton("Geri");
+		btnGeri.setBounds(181, 254, 65, 23);
+		btnGeri.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				dispose();
+			}
+		});
+		btnGeri.setForeground(new Color(255, 255, 255));
+		btnGeri.setBackground(new Color(28, 32, 40));
+		panel.setLayout(null);
+		panel.add(lblUsername_1);
+		panel.add(lblUsername);
+		panel.add(btnGeri);
+		panel.add(usernameField);
+		panel.add(passwordField);
+		panel.add(btnGiris);
+		
+		JFormattedTextField lblAdmin = new JFormattedTextField();
+		lblAdmin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdmin.setText("Admin Giri\u015Fi");
+		lblAdmin.setForeground(new Color(255, 255, 255));
+		lblAdmin.setEditable(false);
+		lblAdmin.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lblAdmin.setBackground(new Color(153, 0, 51));
+		lblAdmin.setBounds(81, 67, 236, 57);
+		panel.add(lblAdmin);
+	}
+}
---
+++ TetraCorpMain.java
@@ -0,0 +1,220 @@
+package DatabaseProject;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JComboBox;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JPasswordField;
+import javax.swing.JInternalFrame;
+import javax.swing.JFormattedTextField;
+import java.awt.Color;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JTabbedPane;
+import java.awt.GridBagLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JToggleButton;
+import java.awt.SystemColor;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.Box;
+import javax.swing.JSpinner;
+import javax.swing.DefaultComboBoxModel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Date;
+
+import javax.swing.UIManager;
+
+@SuppressWarnings("unused")
+public class TetraCorpMain {
+
+	private JFrame frameMain;
+	private BuyTicket buy;
+	private CheckTicket check;
+	private JTextField selection;
+	private JTextField infoTxt;
+	private Admin admin;
+	private RouteStatistics rs;
+	private Date date;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TetraCorpMain window = new TetraCorpMain();
+					window.frameMain.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TetraCorpMain() {
+		//run database here
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frameMain = new JFrame();
+		frameMain.setTitle("Main Screen - Tetra Corporation");
+		frameMain.setBounds(100, 100, 700, 519);
+		frameMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frameMain.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.windowBorder);
+		panel.setBounds(0, 0, 684, 480);
+		frameMain.getContentPane().add(panel);
+		
+		JFormattedTextField corp = new JFormattedTextField();
+		corp.setBorder(null);
+		corp.setForeground(new Color(0, 0, 0));
+		corp.setBackground(new Color(153, 0, 51));
+		corp.setEditable(false);
+		corp.setBounds(0, 0, 684, 159);
+		corp.setFont(new Font("Monospaced", Font.PLAIN, 27));
+		corp.setHorizontalAlignment(SwingConstants.CENTER);
+		corp.setText("Tetra Corporation");
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(28, 32, 40));
+		panel_1.setBounds(0, 160, 684, 320);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(63, 72, 204));
+		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+		gl_panel_1.setHorizontalGroup(
+			gl_panel_1.createParallelGroup(Alignment.TRAILING)
+				.addComponent(panel_2, GroupLayout.DEFAULT_SIZE, 684, Short.MAX_VALUE)
+		);
+		gl_panel_1.setVerticalGroup(
+			gl_panel_1.createParallelGroup(Alignment.TRAILING)
+				.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup()
+					.addContainerGap(72, Short.MAX_VALUE)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 248, GroupLayout.PREFERRED_SIZE))
+		);
+		
+		JComboBox<String> comboSlct = new JComboBox<String>();
+		
+		comboSlct.setMaximumRowCount(4);
+		
+		comboSlct.setModel(new DefaultComboBoxModel<String>(new String[] {"", "Bilet Sat\u0131n Alma", "Bilet/Otobüs Sorgulama", "G\u00FCzergah \u0130statistikleri"}));
+		comboSlct.setSelectedIndex(0);
+		comboSlct.setBackground(new Color(28, 32, 40));
+		comboSlct.setForeground(new Color(255, 255, 255));
+		JButton btnTmm = new JButton("Tamam");
+		btnTmm.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(comboSlct.getSelectedIndex() == 1) {
+					if(buy == null)
+						buy = new BuyTicket();
+					if(!buy.isVisible()) {
+						buy.setVisible(true);
+					}
+				} else if(comboSlct.getSelectedIndex() == 2) {
+					if(check == null)
+						check = new CheckTicket();
+					if(!check.isVisible()) {
+						check.setVisible(true);
+					}
+					
+				} else if(comboSlct.getSelectedIndex() == 3) {
+					if(rs == null)
+						rs = new RouteStatistics();
+					if(!rs.isVisible()) {
+						rs.setVisible(true);
+					}
+				}
+			}
+		});
+		
+		btnTmm.setBackground(new Color(28, 32, 40));
+		btnTmm.setForeground(new Color(255, 255, 255));
+		JButton btnAdmin = new JButton("Admin Giri\u015Fi");
+		btnAdmin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(admin == null)
+					admin = new Admin();
+				if(!admin.isVisible()) {
+					admin.setVisible(true);
+				}
+			}
+		});
+		btnAdmin.setBackground(new Color(28, 32, 40));
+		btnAdmin.setForeground(new Color(255, 255, 255));
+		
+		selection = new JTextField();
+		selection.setBorder(null);
+		selection.setEditable(false);
+		selection.setBackground(new Color(63, 72, 204));
+		selection.setHorizontalAlignment(SwingConstants.CENTER);
+		selection.setForeground(new Color(0, 0, 0));
+		selection.setText(" L\u00FCtfen Yapmak \u0130stedi\u011Finiz \u0130\u015Flemi Se\u00E7in");
+		selection.setColumns(10);
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap(209, Short.MAX_VALUE)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.TRAILING)
+						.addComponent(selection, 248, 248, 248)
+						.addGroup(gl_panel_2.createParallelGroup(Alignment.TRAILING)
+							.addComponent(comboSlct, 0, 248, Short.MAX_VALUE)
+							.addGroup(gl_panel_2.createSequentialGroup()
+								.addComponent(btnAdmin, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addGap(66)
+								.addComponent(btnTmm, GroupLayout.PREFERRED_SIZE, 91, GroupLayout.PREFERRED_SIZE))))
+					.addGap(227))
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(selection, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addComponent(comboSlct, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(40)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnTmm, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnAdmin, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(68, Short.MAX_VALUE))
+		);
+		panel_2.setLayout(gl_panel_2);
+		panel_1.setLayout(gl_panel_1);
+		panel.setLayout(null);
+		
+		infoTxt = new JTextField();
+		infoTxt.setBorder(null);
+		infoTxt.setText("Otob\u00FCs Bilet Sat\u0131n Alma - Sorgulama Platformu");
+		infoTxt.setHorizontalAlignment(SwingConstants.CENTER);
+		infoTxt.setForeground(Color.BLACK);
+		infoTxt.setEditable(false);
+		infoTxt.setColumns(10);
+		infoTxt.setBackground(new Color(153, 0, 51));
+		infoTxt.setBounds(204, 122, 275, 37);
+		panel.add(infoTxt);
+		panel.add(corp);
+		panel.add(panel_1);
+	}
+}
